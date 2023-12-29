c84331ae67c5954ace36ef698c947e9995f8ae6d
0mar99 <omarantonio9972@gmail.com>
2023-12-28 23:38:36 -0600

    vili: Orion-ify!
    Signed-off-by: 0mar99 <omarag9099@gmail.com>

--- orion_vili.mk
+++ orion_vili.mk
@@ -8,17 +8,30 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from haydn device
+# Inherit from vili device
 $(call inherit-product, device/xiaomi/vili/device.mk)
 
-# Inherit some common Lineage stuff.
+# Inherit some common OrionOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Orion
+ORION_MAINTAINER := 0mar
+ORION_BUILD_TYPE := Official
+ORION_MAINTAINER_LINK := https://t.me/ViliSupport
+ORION_GAPPS := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_HAS_UDFPS := false
+
+# Extras
+TARGET_ENABLE_BLUR := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_QUICK_TAP := true
 
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := vili
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := 2107113SG
-PRODUCT_NAME := lineage_vili
+PRODUCT_NAME := orion_vili
 
 PRODUCT_SYSTEM_NAME := vili_global
 PRODUCT_SYSTEM_DEVICE := vili
